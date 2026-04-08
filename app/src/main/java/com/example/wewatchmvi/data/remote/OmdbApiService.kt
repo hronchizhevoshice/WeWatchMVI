9e5279e89851b7211b47ffeb5b69d861c47d06bb
hronchizhevoshice <hronchizhevoshice@gmail.com>
2026-04-08 15:40:14 +0700

    Update omdbAPI

--- app/src/main/java/com/example/wewatchmvi/data/remote/OmdbApiService.kt
+++ app/src/main/java/com/example/wewatchmvi/data/remote/OmdbApiService.kt
@@ -1,7 +1,7 @@
 package com.example.wewatchmvi.data.remote
 
-import com.example.wewatchmvi.domain.model.MovieDetails
-import com.example.wewatchmvi.domain.model.SearchResponse
+import com.example.wewatchmvi.data.remote.model.MovieDetailsDto
+import com.example.wewatchmvi.data.remote.model.SearchResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -10,13 +10,12 @@ interface OmdbApiService {
     suspend fun searchMovies(
         @Query("apikey") apiKey: String,
         @Query("s") searchTerm: String,
-        @Query("y") year: String? = null,
-        @Query("type") type: String = "movie"
-    ): SearchResponse
+        @Query("y") year: String? = null
+    ): SearchResponseDto
 
     @GET("/")
     suspend fun getMovieDetails(
         @Query("apikey") apiKey: String,
         @Query("i") imdbID: String
-    ): MovieDetails
+    ): MovieDetailsDto
 }
